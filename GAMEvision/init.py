480a7ef4fde6fa30a2316d1818239ac2608a33d6
Om Shah <omamitshah@gmail.com>
2025-08-13 11:02:34 +0530

    Added initial files

---
+++ GAMEvision/init.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+model=YOLO('yolov8x')
+result=model.predict(r"input_videos\08fd33_4.mp4",save=True)
+print(result[0])
+print("=======================================")
+for box in result[0].boxes:
+    print(box)
+    
+
